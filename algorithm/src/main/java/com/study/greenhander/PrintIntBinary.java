fa3854abc39a3168600151d2f19ee99b95a5a532
wj2wml <wj2wml@qq.com>
2021-03-10 18:05:51 +0800

    算法基础

---
+++ algorithm/src/main/java/com/study/greenhander/PrintIntBinary.java
@@ -0,0 +1,29 @@
+package com.study.greenhander;
+
+/**
+ * 打印Int数对应的二进制数据, int 32位
+ * 负数：除了符号位，其他取反 后 + 1
+ */
+public class PrintIntBinary {
+    public static void main(String[] args) {
+        int a = 5133222;
+        int b = 150335;
+//        print(a);
+//        print(b);
+//        System.out.println("===================================");
+//        print(a | b);
+//        print(a & b);
+//        print(a ^ b);
+        print(~ 0 + 1);
+
+        // 11111111111111111111111111110101
+        // 10000000000000000000000000001011
+    }
+
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+}
